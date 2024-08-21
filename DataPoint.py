da1838375a05c749e8c1bf81112923bc7e126686
Maxim Reynvoet <maximreynvoet@outlook.com>
2024-08-21 18:45:33 +0200

    Add DataPoint class

---
+++ DataPoint.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Token:
+    ...
+
+@dataclass
+class Person:
+    ...
+
+@dataclass
+class DataPoint:
+    prev_tokens: List[Token]
+    current_token: Token
+    current_talker: Person
+    previous_talker: Person
+    time_talked: int
