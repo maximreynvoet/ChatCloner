b3c746e2414422040bb08735315c304182852f28
vico_ptp <victor-corne@hotmail.com>
2024-08-22 10:32:50 +0200

    Change visibility in MLInterface

--- src/machine_learning/MLInterface.py
+++ src/machine_learning/MLInterface.py
@@ -22,31 +22,31 @@ class MLInterface:
         self._nb_meta_features = ... # Nb people talking + 1 (for how long)
     
     def datapoint_to_bow_input(self, dp: DataPoint) -> Tensor:
-        token_tensors = [self.token_to_tensor(x) for x in dp.prev_tokens]
+        token_tensors = [self._token_to_tensor(x) for x in dp.prev_tokens]
         # TODO this is untested lol
         token_sum = torch.sum(torch.stack(token_tensors), dim=0) 
-        meta_tensor = self.datapoint_to_meta_feature(dp)
+        meta_tensor = self._datapoint_to_meta_feature(dp)
         return torch.concat([token_sum, meta_tensor])
 
     def datapoint_to_output(self, dp: DataPoint) -> Tensor:
         if dp.is_new_person_talking(): 
-            return torch.concat([self.get_empty_token_tensor(), self.datapoint_to_meta_feature(dp)])
+            return torch.concat([self._get_empty_token_tensor(), self._datapoint_to_meta_feature(dp)])
         else:
-            return torch.concat([self.token_to_tensor(dp.current_token), self.get_empty_meta_tensor()])
+            return torch.concat([self._token_to_tensor(dp.current_token), self._get_empty_meta_tensor()])
     
-    def token_to_tensor(self, token: Token) -> Tensor:
+    def _token_to_tensor(self, token: Token) -> Tensor:
         "One hot encoding van tensor op de plaats van de token"
         t = torch.zeros(self._nb_tokens)
         t[token] = 1
         return t
     
-    def get_empty_token_tensor(self) -> Tensor:
+    def _get_empty_token_tensor(self) -> Tensor:
         return torch.zeros(self._nb_tokens)
     
-    def get_empty_meta_tensor(self) -> Tensor:
+    def _get_empty_meta_tensor(self) -> Tensor:
         return torch.zeros(self._nb_meta_features)
 
-    def datapoint_to_meta_feature(self, dp: DataPoint) -> Tensor:
+    def _datapoint_to_meta_feature(self, dp: DataPoint) -> Tensor:
         """Returns a tensor that contains all features but the previous tokens.
         This includes the time talked, and who the current talker is"""
         talker_int = ... # TODO swarchen
