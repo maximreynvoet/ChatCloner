f6ab7c1547711f4c38c0918db2a146f78704b36b
vico_ptp <victor-corne@hotmail.com>
2024-08-22 09:00:06 +0200

    Add base for ML

---
+++ src/machine_learning/machine_learning.py
@@ -0,0 +1,38 @@
+from typing import List
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+"""
+Bag of words model (simpelst)
+
+transformer (overfitting / niet genoeg data)
+
+LSTM
+RNN
+"""
+
+
+"TODO: Als finetuning gebruiken: https://huggingface.co/TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
+class TextPredictor:
+    "Superclass that offers methods to predict token, be able to train etc..."
+    
+    def __init__(self, nb_features_in: int, nb_features_out: int) -> None:
+        self._nb_features_in  = nb_features_in
+        self._nb_features_out = nb_features_out
+
+class BoWModel(TextPredictor, nn.Module):
+    "Simple bag of words model"
+
+    def __init__(self, hidden_layer_sizes: List[int], *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        layers = [self._nb_features_in] + hidden_layer_sizes
+        self._hidden_fc_layers = [nn.Linear(in_features= x, out_features=y) for x, y in zip(layers[:-1], layers[1:])]
+        self._out_layer = nn.Linear(hidden_layer_sizes[-1], self._nb_features_out)
+
+    def forward(self, x):
+        for layer in self._hidden_fc_layers:
+            x = layer(x)
+        x = self._out_layer(x)
+        return F.softmax(x)
