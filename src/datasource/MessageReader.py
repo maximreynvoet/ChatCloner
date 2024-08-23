e18c2fcb39b04a17fb44555e95b9b9cdd7d86584
Maxim Reynvoet <maximreynvoet@outlook.com>
2024-08-24 00:48:58 +0200

    Add filter for connection message + reverse reading for unencrypted messages

--- src/datasource/MessageReader.py
+++ src/datasource/MessageReader.py
@@ -3,7 +3,6 @@ from datatypes.Message import Message
 import json
 from typing import List, Union
 
-
 class MessageReader:
     @staticmethod
     def read_messages_from_json(json_file: str) -> List[Message]:
@@ -15,14 +14,25 @@ class MessengerMessageReader(MessageReader):
     def _json_element_to_msg(json_msg: dict) -> Union[Message, None]: #TODO: use optional, union with None is not recommended (https://peps.python.org/pep-0484/#union-types)
         "Returns a message object based on the json element, if valid (is only text), otherwise None"
         return Message(json_msg["text"], json_msg["senderName"]) if json_msg["type"] == "text" else None
-
+    
     @staticmethod
-    def read_messages_from_json(json_file: str) -> List[Message]:
+    def _read_messages_from_json_ftl(json_file: str) -> List[Message]:
         with open(json_file, "r") as f:
             d = json.load(f)
             l = [MessengerMessageReader._json_element_to_msg(msg) for msg in d["messages"]]
-            return [x for x in l if x is not None]
+            return [x for x in l if x is not None and x.content != "You are now connected on Messenger"] # Old to new (filter out the "You are now connected" message)
         
+    @staticmethod
+    def _read_messages_from_json_ltf(json_file: str) -> List[Message]:
+        with open(json_file, "r") as f:
+            d = json.load(f)
+            l = [MessengerMessageReader._json_element_to_msg(msg) for msg in d["messages"]]
+            return [x for x in l[::-1] if x is not None and x.content != "You are now connected on Messenger"] # New to old (filter out the "You are now connected" message)
+
+    @staticmethod
+    def read_messages_from_json(json_file: str) -> List[Message]:
+        return MessengerMessageReader._read_messages_from_json_ftl(json_file)
+
 
 class DiscordMessageReader(MessageReader):
     @staticmethod
@@ -33,5 +43,5 @@ class DiscordMessageReader(MessageReader):
     def read_messages_from_json(json_file: str) -> List[Message]:
         with open(json_file, "r") as f:
             d = json.load(f)
-            l = [DiscordMessageReader._json_element_to_msg(msg) for msg in d["messages"]]
+            l = [DiscordMessageReader._json_element_to_msg(msg) for msg in d["messages"]] # Old to new
             return [x for x in l if x is not None]
