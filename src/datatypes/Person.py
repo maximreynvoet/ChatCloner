a39770d50778d1a44ac4695cfc048f22e42a4f83
vico_ptp <victor-corne@hotmail.com>
2024-09-09 22:36:53 +0200

    Person mapping to from int

--- src/datatypes/Person.py
+++ src/datatypes/Person.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from tenacity import retry
+
 from datatypes.Platform import Platform
 
 """
@@ -16,6 +18,24 @@ class Person(Enum):
     NICK = "Nick"
     UNKNOWN = "Unknown"
     "TODO Ook een methode om dit van en naar getal te translaten (nodig voor one hot in NN)"
+
+    @staticmethod
+    def _int_mapping() : 
+        "Has to be done via method, otherwise this mapping will be seen as a field -> instance of 'a person'"
+        return {
+            Person.MAXIM:   1,
+            Person.VICTOR:  2,
+            Person.NICK:    3,
+            Person.UNKNOWN: 4,
+        }
+
+    @staticmethod
+    def get_nb_persons() -> int:
+        return len(list(Person))
+    
+    @staticmethod
+    def get_intvalue_for_unknown() -> int:
+        return Person.get_nb_persons()
 
     @classmethod
     def string_to_person(cls, sender : str, platform : Platform) -> 'Person':
@@ -34,3 +54,11 @@ class Person(Enum):
             # Facebook Messenger uses full names
             firstname = sender.split(" ")[0]
             return (cls(firstname) if firstname in ["Maxim","Victor", "Nick"] else Person.UNKNOWN)
+
+    @staticmethod
+    def person_to_int(person: "Person") -> int:
+        return Person._int_mapping().get(person, Person.get_intvalue_for_unknown())
+        
+    @staticmethod
+    def int_to_person(number: int) -> "Person":
+        return next((k for k,v in Person._int_mapping().items() if v == number), Person.UNKNOWN)
