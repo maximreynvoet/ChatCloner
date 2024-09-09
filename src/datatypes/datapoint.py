d25b32315e83468e83c6a5099b13b28c20a0b03d
vico_ptp <victor-corne@hotmail.com>
2024-09-09 19:11:00 +0200

    Paar todos op datapoint generator

--- src/datatypes/datapoint.py
+++ src/datatypes/datapoint.py
@@ -22,6 +22,26 @@ class ConversationParser:
     conversation: List[Message]
 
     def parse(self, N : int, tokenizer : Tokenizer) -> List[DataPoint]:
+        """TODO afhankelijk van hoeveel datapunten er zijn => denken aan een iterator-based aanpak
+
+        Sommige NN-aanpakken kunnen meer / beter leren als de voorbeelden at random gegeven worden
+
+        Bedenk de tradeoff tss memory, learning, etc
+
+        ---
+
+        TODO Ook kan er van een datapunt met $n$ tokens $n$ trainings-voorbeelden gegenereerd worden.
+        Denken waar deze eigenschap gebruikt zou worden (hier zelf genereren ? In ML? In database? In de API die get_datapoint_example() genereert ?)
+        
+        TODO Een endpoint / provider classe die get_datapoint() kan aanbieden. Deze kan op zijn beurt kiezen voor sequentieel te genereren of alles op te slaan en at random te providen
+
+        TODO (indien reworking nodig): vervangen door functional approach (zie onderstaand messenger bericht):
+        Niet ik die in mijn slaap een puur functional approach van map reduce heb gevonden 😅
+        Map messages naar tuple van <token, author>, neem sliding window van size n erop, en vanuit die sliding window kan je bepalen wie de auteur is
+        Aja fack kan niet weten voor hoe lang de current talker aanwezig was, alhoewel toch wel met een incremental iets
+        Update: ja je zou wel incrementeel iets kunnen doen voor duur hoe lang spreker aanwezig is (gwn zijnde vorige count + 1 als talkers zelfde, anders 1)
+        
+        """
         context_window : List[Token] = []
         conversation_tokens : List[DataPoint] = []
         prev_talker : Optional[Person] = None
