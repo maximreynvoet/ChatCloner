8ab640312edc6445e5b304e75beca183c58aca83
vico_ptp <victor-corne@hotmail.com>
2024-09-10 22:54:53 +0200

    Init MixedIterator

--- src/utils/MixedIterator.py
+++ src/utils/MixedIterator.py
@@ -2,28 +2,31 @@
 import random
 from typing import Iterator, List, TypeVar
 
+from attr import dataclass
+
 T = TypeVar("T")
 
 
+@dataclass
 class MixedIterator(Iterator[T]):
     """This is an iterator that is a collection of multiple iterators.
     When retrieving a value from this iterator: it returns the value of one of its iterators at random (if still available), or returns a StopIteration if all are empty"""
-    _iterators: List[Iterator[T]]
+    iterators: List[Iterator[T]]
 
     def __next__(self) -> T:
-        if not self._iterators:
+        if not self.iterators:
             raise StopIteration  # All providers are exhausted
 
         # Randomly select a provider
-        while self._iterators:
-            provider = random.choice(self._iterators)
+        while self.iterators:
+            provider = random.choice(self.iterators)
 
             try:
                 # Try to get the next datapoint from the chosen provider
                 return next(provider)
             except StopIteration:
                 # If the provider is exhausted, remove it from the list
-                self._iterators.remove(provider)
+                self.iterators.remove(provider)
 
         # If all providers are exhausted, raise StopIteration
         raise StopIteration
