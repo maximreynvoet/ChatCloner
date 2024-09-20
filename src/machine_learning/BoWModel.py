71a8a333f9ee3d332b813daa1b698f6506d10cf3
vico_ptp <victor-corne@hotmail.com>
2024-09-20 11:53:01 +0200

    BOWModelFactory

--- src/machine_learning/BoWModel.py
+++ src/machine_learning/BoWModel.py
@@ -11,7 +11,6 @@ import torch.nn.functional as F
 from typing import List
 
 from machine_learning.fully_connected import FullyConnectedModule
-from utils.sequences import Sequences
 from utils.utils import Utils
 
 
@@ -26,8 +25,7 @@ class BoWModel(PytorchTextPredictor):
                  people_hidden: FullyConnectedModule,
                  siamese_hidden: FullyConnectedModule,
                  token_out: FullyConnectedModule,
-                 people_out: FullyConnectedModule,
-                 *args, **kwargs) -> None:
+                 people_out: FullyConnectedModule) -> None:
         "TODO de type hinting kan beter zijn (mss FullyConnected -> nn.Module (als dat goed genoeg is))"
         self._nb_tokens = nb_tokens
         self._nb_people = nb_people
@@ -36,53 +34,6 @@ class BoWModel(PytorchTextPredictor):
         self._siamese_hidden = siamese_hidden
         self._token_out = token_out
         self._people_out = people_out
-
-    @staticmethod
-    def get_default_epmty_instance(nb_tokens: int, nb_people: int) -> "BoWModel":
-        """Returns an instance of a BoWModel
-        
-        TODO -> factory method
-        In de factory: gemakkelijk hyperparameters toevoegen (om hyperparameter optimization te doen)
-            Deze params hier: mogelijks te veel, mogelijks te weinig, geen idee /shrug
-            -V 2024-09-19
-
-        TODO mss ook betere API om fullyconnected te maken 
-            - FullyConnected.FromSequence(start, nb_repeats)
-            - FullyConnected.FromPowerLaw(stars, end, power, (max_len))
-
-        TODO BOW FActory comment over hyperparam search door de bayesian optimization
-            - https://en.wikipedia.org/wiki/Neural_architecture_search
-            - Bayesian Optimization
-            - random search
-            - grid search
-            - genetic algorithm (als echt yolo)
-
-        nb_tokens: ongeveer 256 ish
-        nb_people: 4-8
-        """
-
-        token_siamese_in = 64
-        people_siamese_in = nb_people
-        people_repeat = 4
-        siamese_repeat = 4
-        siamese_in = token_siamese_in + people_siamese_in
-        siamese_out = siamese_in
-        
-        return BoWModel(nb_tokens=nb_tokens,
-                        nb_people=nb_people,
-
-                        token_hidden=   FullyConnectedModule(Sequences.power_sequence(nb_tokens, token_siamese_in, 0.75)),
-                        people_hidden=  FullyConnectedModule(Sequences.repeat_sequence(nb_people, people_repeat)),
-                        siamese_hidden= FullyConnectedModule(Sequences.repeat_sequence(siamese_in, siamese_repeat)),
-                        people_out=     FullyConnectedModule(Sequences.power_sequence(siamese_out, nb_people, 0.5)),
-                        token_out=      FullyConnectedModule(Sequences.power_sequence(siamese_out, nb_tokens, 0.5))
-                        
-                        token_hidden=   FullyConnectedModule(nb_tokens, Utils.reduce_sequence_power(nb_tokens, token_siamese_in, 0.75)),
-                        people_hidden=  FullyConnectedModule(nb_people, [nb_people] * people_repeat),
-                        siamese_hidden= FullyConnectedModule(siamese_in, [siamese_in] * siamese_repeat),
-                        people_out=     FullyConnectedModule(siamese_out, Utils.reduce_sequence_power(siamese_out, nb_people, 0.5)),
-                        token_out=      FullyConnectedModule(siamese_out, Utils.reduce_sequence_power(siamese_out, nb_tokens, 0.5))
-                        )
 
     def forward(self, x: BOWInputTensor) -> BOWOutputTensor:
         """TODO kan ik gwn fullyConnected callen ?"""
@@ -105,3 +56,5 @@ class BoWModel(PytorchTextPredictor):
 
     def train_model(self) -> None:
         ...
+
+
