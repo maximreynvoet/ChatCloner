51ab302f097593354128254c17878ff74b68203a
vico_ptp <victor-corne@hotmail.com>
2024-09-20 19:58:16 +0200

    Data location

--- src/datasource/JsonRepo.py
+++ src/datasource/JsonRepo.py
@@ -1,16 +1,16 @@
 import os
 from typing import List
 
-
+_CHAT_FOLDER = "../data/Chats/"
 
 class JsonRepo:
     @staticmethod
     def get_all_messenger_json_chat_paths() -> List[str]:
-        return JsonRepo._get_all_json_files_in_folder("../Messenger")
+        return JsonRepo._get_all_json_files_in_folder(_CHAT_FOLDER + "Messenger")
     
     @staticmethod
     def get_all_discord_json_chats_paths() -> List[str]:
-        return JsonRepo._get_all_json_files_in_folder("../Discord")
+        return JsonRepo._get_all_json_files_in_folder(_CHAT_FOLDER + "Discord")
 
     @staticmethod
     def _get_all_json_files_in_folder(folder: str) -> List[str]:
