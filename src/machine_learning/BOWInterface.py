be26eb1590f4b60554dca7128693197d2a79426b
vico_ptp <victor-corne@hotmail.com>
2024-09-22 11:26:06 +0200

    Combined train observers + save observers

--- src/machine_learning/BOWInterface.py
+++ src/machine_learning/BOWInterface.py
@@ -1,31 +1,18 @@
-from tqdm import tqdm
-from datasource.datapoint_provider import DatapointProvider
 from datatypes.MessageFragment import MessageFragment
-from datatypes.Person import Person, PersonManager
 from datatypes.datapoint import DataPoint
-from datatypes.tensors.pure_tensors import OneHotTensor
-from datatypes.tensors.use_case_tensors import TalkerTensor, TokenCountTensor
-from datatypes.tensors.ml_tensors import BOWInputTensor, BOWOutputTensor, MLInputTensor, MLOutputTensor
+from datatypes.tensors.ml_tensors import BOWInputTensor, MLInputTensor, MLOutputTensor
 from machine_learning.BoWModel import BoWModel
 from machine_learning.MLInterface import MLInterface
 
-import torch
-import torch.optim as optim
-
-from utils.utils import Utils
-
 
 class BOWInterface(MLInterface):
-    """TODO Het is niet omdat nu alle methodes weg zijn dat deze classe voor niets dient. Gebruik het om UI te doen, prompt en van text in -> text uit
-    """
-
     def __init__(self, model: BoWModel) -> None:
         super().__init__()
         self._model = model
 
     def _generate_next_input(self, prev_input: MLInputTensor, out_fragment: MessageFragment) -> MLInputTensor:
         return BOWInputTensor.from_previous_output(prev_input, out_fragment)
-    
+
     def _dp_to_model_in(self, dp: DataPoint) -> MLInputTensor:
         return BOWInputTensor.from_datapoint(dp)
 
--- src/machine_learning/train_watcher.py
+++ src/machine_learning/train_watcher.py
@@ -1,9 +1,11 @@
 
+import os
 from typing import Collection
 from datatypes.Conversation import Conversation
 from machine_learning.BOWInterface import BOWInterface
 from machine_learning.TextPredictor import PytorchTextPredictor
 from machine_learning.predict_convo_params import PredictConvoParams
+from utils.examples import Examples
 from utils.saving import Saving
 
 
@@ -11,7 +13,6 @@ from utils.saving import Saving
 TODO ik ben niet zoooo blij dat het een pytorchTextPredictor zou moeten zijn als param
 TODO ook ene voor model te saven (checkpointen)
 """
-
 
 class TrainingObserver:
     def combined_with(self, other: 'TrainingObserver') -> 'TrainingObserver':
@@ -23,8 +24,15 @@ class TrainingObserver:
         Subclasses could implement this to for example see what the model would predict on a test-example"""
         pass
 
+    @staticmethod
+    def get_default_train_observers(model_name: str, interface: BOWInterface, frequency: int) -> 'TrainingObserver':
+        return CombinedTrainingsObserver([
+            TestModelObserver.get_default_instance(model_name, interface, frequency),
+            SaveModelObserver.get_default_instance(model_name, frequency)
+        ])
+
 class CombinedTrainingsObserver(TrainingObserver):
-    
+
     def __init__(self, observers: Collection[TrainingObserver]) -> None:
         super().__init__()
         self._observers = observers
@@ -32,14 +40,15 @@ class CombinedTrainingsObserver(TrainingObserver):
     def update(self, model) -> None:
         [m.at_new_training_instance(model) for m in self._observers]
 
-class ContinueConvoObserver(TrainingObserver):
+
+class TestModelObserver(TrainingObserver):
     "Class that continues a test conversation every n trainings examples"
 
-    def __init__(self, 
+    def __init__(self,
                  interface: BOWInterface,
-                 activation_frequency: int, 
-                 save_file: str, 
-                 test_convo: Conversation, 
+                 activation_frequency: int,
+                 save_file: str,
+                 test_convo: Conversation,
                  prediction_params: PredictConvoParams) -> None:
         super().__init__()
         self._counter = 0
@@ -49,16 +58,42 @@ class ContinueConvoObserver(TrainingObserver):
         self._test_convo = test_convo
         self._prediction_params = prediction_params
 
+    @staticmethod
+    def get_default_instance(model_name: str, interface: BOWInterface, frequency: int) -> 'TestModelObserver':
+        return TestModelObserver(interface,
+                                     frequency,
+                                     f"../output/{model_name}",
+                                     Examples.example_convo,
+                                     PredictConvoParams.get_default_params())
+        ...
+
     def at_new_training_instance(self, model: PytorchTextPredictor) -> None:
         self._counter += 1
-        if self._counter % self._activation_frequency == 0: self._test_save_convo(model)
+        if self._counter % self._activation_frequency == 0:
+            self._test_save_convo(model)
 
     def _test_save_convo(self, model: PytorchTextPredictor):
         "Tests the model on this conversation, and saves result to file"
-        # TODO de model zit al in de interface. Niet goed, niet demure. 
+        # TODO de model zit al in de interface. Niet goed, niet demure.
         # Zetten we dit als precondition van init ? Een interface die de model niet nodig heeft?
-        conv = self._interface.predict_convo_continuation(self._prediction_params, self._test_convo)
+        conv = self._interface.predict_convo_continuation(
+            self._prediction_params, self._test_convo)
         s = f"Model at iteration {self._counter}: \n\n\n {str(conv)} \n\n\n{'='*50}"
         Saving.write_str_to_file(s, self._save_file)
 
 
+class SaveModelObserver(TrainingObserver):
+    def __init__(self, activation_frequency: int, save_dir: str) -> None:
+        super().__init__()
+        self._counter = 0
+        self._activation_freq = activation_frequency
+        self._save_dir = save_dir
+
+    def at_new_training_instance(self, model: PytorchTextPredictor) -> None:
+        self._counter += 1
+        if self._counter % self._activation_freq == 0: 
+            Saving.save_bow_model(model, os.path.join(self._save_dir, f"Model_iter_{self._counter}.pth"))
+    
+    @staticmethod
+    def get_default_instance(model_name: str, frequency: int) -> 'SaveModelObserver':
+        return SaveModelObserver(frequency, f"../output/{model_name}")
